b094311b8f62a56116948cefb6c2c4b391e9eb46
jsromero98 <84394100+jsromero98@users.noreply.github.com>
2022-05-12 15:49:31 -0500

    agregado routing a login y signin

--- unpdf_wa/src/App.js
+++ unpdf_wa/src/App.js
@@ -1,19 +1,35 @@
 import './App.css';
-import { Link } from "react-router-dom";
+import ParticlesBg from 'particles-bg';
+import { Card } from 'primereact/card';
+import { Button } from 'primereact/button';
+import { useNavigate } from "react-router-dom";
+
 
 export default function App() {
+  let navigate = useNavigate();
   return (
     <div>
-      <h1>Landing!</h1>
-      <nav
-        style={{
-          borderBottom: "solid 1px",
-          paddingBottom: "1rem",
-        }}
-      >
-        <Link to="/login">Login</Link> |{" "}
-        <Link to="/signup">Signup</Link>
-      </nav>
-    </div>
+      <ParticlesBg type="circle" bg={true} />
+      <div className='flex justify-content-center fullHeight m-5'>
+        <Card >
+          <div  className='flex justify-content-center'>
+            <img src='/unpdf.svg' className='landinglogo'/>
+          </div>
+          <div className='flex justify-content-center'>
+            <Button label="Log in" className='m-2' onClick={toLogin}/>
+            <Button label="Sign in" className='m-2' onClick={toSignin}/>
+          </div>
+        </Card>
+      </div>
+    </div> 
   );
+
+  function toSignin(){
+
+  }
+  
+  function toLogin(){
+    navigate("/login");
+  }
+  
 }
--- unpdf_wa/src/index.js
+++ unpdf_wa/src/index.js
@@ -7,6 +7,11 @@ import reportWebVitals from './reportWebVitals';
 import Login from './Pages/Login';
 import Signup from './Pages/Signup';
 
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";                                //icons
+import "primeflex/primeflex.css";                                //primeflex
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
