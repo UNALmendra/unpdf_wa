aa72bc08f69bc2ca20a5b8b97eaf9024163c549f
Juan Sierra <jgsierraa@unal.edu.co>
2022-05-19 00:36:11 -0500

    fix: quitar el quemado de datos en upload file

--- unpdf_wa/src/Pages/MyDocs/MyDocs.js
+++ unpdf_wa/src/Pages/MyDocs/MyDocs.js
@@ -17,7 +17,7 @@ export default function MyDocs() {
         var full_type = files["fileList"][0]["type"];
         var name = full_name.substring(0, full_name.lastIndexOf('.'));
         var type = full_type.substring(full_type.lastIndexOf('/') + 1);
-        var user = "User react parametro"
+        var user = localStorage.getItem("id")
         var base64_data = files["base64"]
         var file = base64_data.substring(base64_data.indexOf(",") + 1)
         uploadDocuments(name, type, user, file);
